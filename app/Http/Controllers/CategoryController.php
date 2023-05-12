cd27aca23599701ce097cfbfe6dfaa5b67f6610d
programming-Account <keiaki.120388@gmail.com>
2023-05-12 12:55:10 +0900

    Breeze認証機能作成

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,6 +9,7 @@ class CategoryController extends Controller
 {
     public function index(Category $category)
     {
+        // dd($category);
         return view('categories.index')->with(['posts' => $category->getByCategory()]);
-    }
+    }//この'posts'の中身が知りたい。共通のカテゴリーを持ったpostsテーブルのデータも入っているのか？
 }
